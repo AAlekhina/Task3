888362b1c1696359501777828af49216b81d2ee7
AAlekhina <ae.alekhina@gmail.com>
2024-09-06 16:16:04 +0200

    modified 6/09/24

--- Query.sql
+++ Query.sql
@@ -1,10 +1,12 @@
 # 1
-SELECT category.name as Category_name, count(film_category.film_id) as Count_of_film
-    FROM film_category
-    JOIN category
+SELECT
+    category.name AS category_name,
+    count(film_category.film_id) AS Count_of_film
+FROM film_category
+INNER JOIN category
     ON category.category_id = film_category.category_id
-    GROUP BY category.name
-    ORDER BY Count_of_film DESC;
+GROUP BY category.name
+ORDER BY count_of_film DESC;
 
 #2
 SELECT
@@ -12,13 +14,13 @@ SELECT
     a.first_name || ' '|| a.last_name AS name_of_actor,
     COUNT(r.rental_id) AS rental_counter
 FROM actor a
-JOIN
+INNER JOIN
     film_actor film_actor ON a.actor_id = film_actor.actor_id
-JOIN
+INNER JOIN
     film film ON film_actor.film_id = film.film_id
-JOIN
-    inventory i on film.film_id = i.film_id
-JOIN
+INNER JOIN
+    inventory i ON film.film_id = i.film_id
+INNER JOIN
     rental r ON i.inventory_id = r.inventory_id
 GROUP BY a.actor_id, a.first_name, a.last_name
 ORDER BY
@@ -29,13 +31,13 @@ LIMIT 10;
 SELECT
     category.name, sum(payment.amount) AS spent
 FROM category
-JOIN film_category ON
+INNER JOIN film_category ON
     category.category_id = film_category.category_id
-JOIN inventory ON
+INNER JOIN inventory ON
     film_category.film_id = inventory.film_id
-JOIN rental ON
+INNER JOIN rental ON
     inventory.inventory_id = rental.inventory_id
-JOIN payment ON
+INNER JOIN payment ON
     rental.rental_id = payment.rental_id
 GROUP BY category.name
 ORDER BY spent DESC
@@ -44,9 +46,11 @@ LIMIT 1;
 #4
 SELECT film.title
 FROM film
-LEFT JOIN
-    inventory ON film.film_id = inventory.film_id
-WHERE inventory.film_id IS NULL;
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM inventory
+    WHERE inventory.film_id = film.film_id
+);
 
 #5
 WITH  actor_film_count AS (
@@ -54,50 +58,63 @@ WITH  actor_film_count AS (
         actor.first_name || ' ' || actor.last_name AS actor_name,
         count(film_category.film_id) AS film_count
     FROM actor
-    JOIN film_actor
+    INNER JOIN film_actor
         ON actor.actor_id = film_actor.actor_id
-    JOIN film_category
+    INNER JOIN film_category
         ON film_category.film_id = film_actor.film_id
-    Join category
+    INNER JOIN category
         ON film_category.category_id = category.category_id
     WHERE category.name = 'Children'
     GROUP BY actor.first_name, actor.last_name
     ),
-top3_actor AS (
-    SELECT actor_name, film_count
+ranked_actors AS (
+    SELECT
+        actor_name,
+        film_count,
+        RANK() OVER (ORDER BY film_count DESC) AS rank
     FROM actor_film_count
-    ORDER BY film_count DESC
-    LIMIT 3)
+)
 
-SELECT actor_name, film_count
-    FROM actor_film_count
-        WHERE film_count>=(SELECT min(film_count)
-                           FROM top3_actor)
+SELECT
+    actor_name,
+    film_count
+FROM ranked_actors
+WHERE rank <= 3
 ORDER BY film_count DESC;
 
 #6
 SELECT city.city,
-       SUM(CASE WHEN customer.active = 1 THEN 1 ELSE 0 END) AS Active_customers,
-       SUM(CASE WHEN customer.active = 0 THEN 1 ELSE 0 END) AS Inactive_customers
+       SUM(CASE WHEN customer.active = 1 THEN 1 ELSE 0 END) AS active_customers,
+       SUM(CASE WHEN customer.active = 0 THEN 1 ELSE 0 END) AS inactive_customers
 FROM city
-    JOIN address  ON city.city_id = address.city_id
-    JOIN customer ON address.address_id = customer.address_id
+    INNER JOIN address  ON city.city_id = address.city_id
+    INNER JOIN customer ON address.address_id = customer.address_id
 GROUP BY city.city
-ORDER BY Inactive_customers DESC;
+ORDER BY inactive_customers DESC;
 
 # 7
-SELECT category.name, city.city,
-       SUM(EXTRACT(HOUR FROM(rental.return_date-rental.rental_date))) AS rental_hours
-FROM category
-    JOIN film_category ON category.category_id = film_category.category_id
-    JOIN inventory  ON film_category.film_id = inventory.film_id
-    JOIN rental  ON  inventory.inventory_id = rental.inventory_id
-    JOIN customer ON rental.customer_id = customer.customer_id
-    JOIN address ON address.address_id = customer.address_id
-    JOIN city  ON city.city_id = address.city_id
- WHERE
-        city.city ILIKE 'A%' OR city.city LIKE '%-%'
-GROUP BY category.name, city.city
-ORDER BY rental_hours DESC
-LIMIT 1;
+WITH categorized_rentals AS (SELECT category.name AS category_name,
+                                    city.city AS city,
+                                    SUM(EXTRACT(HOUR FROM (rental.return_date - rental.rental_date))) AS rental_hours,
+                                    ROW_NUMBER() OVER (
+                                        PARTITION BY CASE
+                                                         WHEN city.city ILIKE 'A%' THEN 'starts_with_A'
+                                                         WHEN city.city LIKE '%-%' THEN 'contains_dash'
+                                            END
+                                        ORDER BY SUM(EXTRACT(HOUR FROM (rental.return_date - rental.rental_date))) DESC
+                                        )AS rn
+                             FROM category
+                                      INNER JOIN film_category ON category.category_id = film_category.category_id
+                                      INNER JOIN inventory ON film_category.film_id = inventory.film_id
+                                      INNER JOIN rental ON inventory.inventory_id = rental.inventory_id
+                                      INNER JOIN customer ON rental.customer_id = customer.customer_id
+                                      INNER JOIN address ON address.address_id = customer.address_id
+                                      INNER JOIN city ON city.city_id = address.city_id
+                             WHERE city.city ILIKE 'A%'
+                                OR city.city LIKE '%-%'
+                             GROUP BY category.name, city.city)
+SELECT category_name, city, rental_hours, rn
+FROM categorized_rentals
+WHERE rn<=1
+ORDER BY  rental_hours DESC;
 
